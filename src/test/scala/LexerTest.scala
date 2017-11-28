ca9bfcc12a6fa50fe2e8ff889cd88a12744aaf7f
Torstein Vik <torsteinv64@gmail.com>
2017-11-28 21:38:25 +0100

    Added imports to LexerTest

--- src/test/scala/LexerTest.scala
+++ src/test/scala/LexerTest.scala
@@ -1,9 +1,13 @@
 package io.github.torsteinvik.flow.test
 
+import io.github.torsteinvik.flow.parser.lexer._
+
 import org.scalatest.FunSuite
 
 class LexerTest extends FunSuite {
     test ("") {
         
+    import Token._
+    
     }
 }
