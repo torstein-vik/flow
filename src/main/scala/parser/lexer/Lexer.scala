c65b0d94d50791dbed56c92e0dcc549d36d9ba2c
Torstein Vik <torsteinv64@gmail.com>
2017-11-28 21:43:31 +0100

    Added syntactical tokens to getToken

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -11,5 +11,12 @@ object Lexer {
     private def getToken(string : String) : Option[Token] = string match {
         case "Semantic" => Some(Token.Semantic)
         case "Define" => Some(Token.Define)
+        case ":" => Some(Token.Colon)
+        case ";" => Some(Token.Semicolon)
+        case "=>" => Some(Token.Machine)
+        case "->" => Some(Token.Flow)
+        case "(" => Some(Token.TupleOpen)
+        case ")" => Some(Token.TupleClose)
+        case "," => Some(Token.TupleSeparator)
     }
 }
