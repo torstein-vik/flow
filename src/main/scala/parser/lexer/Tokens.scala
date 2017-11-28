418ac779715c97bf4907e8207e5858923356ff14
Torstein Vik <torsteinv64@gmail.com>
2017-11-28 03:14:32 +0100

    Added doc for Token

--- src/main/scala/parser/lexer/Tokens.scala
+++ src/main/scala/parser/lexer/Tokens.scala
@@ -1,6 +1,8 @@
 package io.github.torsteinvik.flow.parser.lexer
 
+/** A token coming from the lexing of the input code */
 abstract sealed class Token ()
 
+/** Object with all the possible tokens */
 object Token {
 }
