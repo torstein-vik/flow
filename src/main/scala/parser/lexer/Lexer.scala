86c2a18dc730166151c2fcfd1c0c972f13ceb9d5
Torstein Vik <torsteinv64@gmail.com>
2017-12-02 03:13:49 +0100

    Added code to split by lines

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -9,7 +9,12 @@ class Lexer (data : String) {
         import scala.collection.mutable.{Seq => MSeq}
         var analysis : MSeq[(Either[String, Token], Int)] = MSeq()
         
-        
+        // Split by lines
+        // TODO: This adds an extra Newline token
+        for ((line, linenum) <- data.split(Token.Newline.chars).zipWithIndex) {
+            analysis = analysis ++ Seq((Left(line), linenum + 1))
+            analysis = analysis ++ Seq((Right(Token.Newline), linenum + 1))
+        }
         
         return for {(Right(token), linenum) <- analysis} yield (token, linenum)
     }
