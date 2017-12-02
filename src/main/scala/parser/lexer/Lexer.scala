4347b34ab4ea5f9847b384ec00e93aacdf7a66be
Torstein Vik <torsteinv64@gmail.com>
2017-12-02 03:13:17 +0100

    Added line number to Lexer.analyze

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -5,13 +5,13 @@ class Lexer (data : String) {
     import Lexer._
     import Token._
     
-    def analyze : Seq[Token] = {
+    def analyze : Seq[(Token, Int)] = {
         import scala.collection.mutable.{Seq => MSeq}
-        var analysis : MSeq[Either[String, Token]] = MSeq(Left(data))
+        var analysis : MSeq[(Either[String, Token], Int)] = MSeq()
         
         
         
-        return for {Right(token) <- analysis} yield token
+        return for {(Right(token), linenum) <- analysis} yield (token, linenum)
     }
 }
 
@@ -19,6 +19,6 @@ object Lexer {
     import Token._
     
     def getTokens(data : String) : Seq[Token] = {
-        return new Lexer(data).analyze
+        return new Lexer(data).analyze.map{case (token, _) => token}
     }
 }
