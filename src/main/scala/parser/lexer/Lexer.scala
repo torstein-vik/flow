6d5422145c7882d62b9766a15d357695f954c461
Torstein Vik <torsteinv64@gmail.com>
2017-12-02 01:01:43 +0100

    Removed getToken in favor of better system with Token.accept

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -10,20 +10,4 @@ object Lexer {
     private val numeric = ('0' to '9').toSet
     
     import Token._
-    private def getToken(string : String) : Option[Token] = string match {
-        
-        
-        case "Semantic" => Some(Semantic)
-        case "Define" => Some(Define)
-        case ":" => Some(Colon)
-        case ";" => Some(Semicolon)
-        case "=>" => Some(Machine)
-        case "->" => Some(Flow)
-        case "(" => Some(TupleOpen)
-        case ")" => Some(TupleClose)
-        case "," => Some(TupleSeparator)
-        case str if str.forall(numeric.contains(_)) => Some(new Identifier(str) with Numeric)
-        case str if str.forall(identifier.contains(_)) => Some(new Identifier(str))
-        case _ => None
-    }
 }
