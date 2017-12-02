08f55a37ccf054f1e35a8ff1a8f8486e436a7df0
Torstein Vik <torsteinv64@gmail.com>
2017-12-02 01:05:42 +0100

    Made Lexer.getTokens point to new method in class Lexer

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -2,12 +2,12 @@ package io.github.torsteinvik.flow.parser.lexer
 
 
 object Lexer {
+    import Token._
     def getTokens(file : String) : Seq[Token] = {
-        return Seq.empty[Token]
+        return new Lexer(file).analyze
     }
     
     
-    import Token._
     private val legalchars = (('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')).toSet ++ Set(' ', '\n', ':', ';', '=', '>', '-', '(', ')', ',')
     
 }
