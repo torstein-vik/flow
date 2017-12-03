ad7dddf35d9721b152179216c856fc3776984bd3
Torstein Vik <torsteinv64@gmail.com>
2017-12-03 14:50:43 +0100

    Added \r? to multilinecomment

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -25,7 +25,7 @@ object Lexer extends RegexParsers {
     def identifier : Parser[Token] = """[A-Za-z0-9]+""".r ^^ (new Identifier(_))
     
     def singlelinecomment : Parser[Token] = """\/\/.*\r?\n""".r ^^ (Comment(_))
-    def multilinecomment : Parser[Token] = """\/\*((?!\*\/).|\n)*\*\/""".r ^^ (Comment(_))
+    def multilinecomment : Parser[Token] = """\/\*((?!\*\/).|\r?\n)*\*\/""".r ^^ (Comment(_))
     
     def total : Parser[Seq[Token]] = (multilinecomment | singlelinecomment | fixedToken | numeric | identifier).*
 }
