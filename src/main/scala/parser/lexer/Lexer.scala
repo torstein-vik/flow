16f2cd210189e5b3021ebec5c5dd2d6bb1c02077
Torstein Vik <torsteinv64@gmail.com>
2017-12-03 04:36:25 +0100

    Removed Lexer class

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -1,13 +1,9 @@
 package io.github.torsteinvik.flow.parser.lexer
 
 
-class Lexer (data : String) {
-    import Lexer._
-    import Token._
-    
-}
 
 object Lexer {
+
     import Token._
     
     def getTokens(data : String) : Seq[Token] = {
