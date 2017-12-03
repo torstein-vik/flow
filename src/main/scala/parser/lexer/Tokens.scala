b837e44a7fa062cf434d83921d34839ce659ce0f
Torstein Vik <torsteinv64@gmail.com>
2017-12-03 03:54:53 +0100

    Removed vals for string delimiter, this is never going to change

--- src/main/scala/parser/lexer/Tokens.scala
+++ src/main/scala/parser/lexer/Tokens.scala
@@ -70,11 +70,9 @@ object Token {
         def value : BigInt = BigInt(name) 
     }
     
-    val stringLiteralStartDelimiter = "\""
-    val stringLiteralEndDelimiter = "\""
     /** String Literal [[Token]] 
      *  @param content the content of this string literal
      */
-    case class StringLiteral (val content : String) extends FixedToken(stringLiteralStartDelimiter + content + stringLiteralEndDelimiter)
+    case class StringLiteral (val content : String) extends FixedToken("\"" + content + "\"")
     
 }
