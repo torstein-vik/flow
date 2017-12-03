622958816d57b7b17183856b7868fb7b6f473e75
Torstein Vik <torsteinv64@gmail.com>
2017-12-03 15:09:18 +0100

    Added doc to Lexer.scala

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -4,10 +4,14 @@ import scala.util.parsing.combinator._
 import scala.util.matching.Regex
 
 
-
+/** Lexer turning some input data into a sequence of [[Token]]s. Use [[getTokens]], as parser doesn't filter away [[Token.Comment]] tokens */
 object Lexer extends RegexParsers {
     import Token._
     
+    /** Return the sequence of [[Token]]s coming from parsing the input data.
+     *  @param data the data to tokenize
+     */
+    @throws[LexerException]("if there is a lexing error or no parse")
     def getTokens(data : String) : Seq[Token] = parseAll(total, data) match {
         case Success(result, _) => result.filter{
             case Comment(_) => false
@@ -18,15 +22,22 @@ object Lexer extends RegexParsers {
         case Error(msg, next) => throw LexerException(msg, next.pos)
     }
     
+    /** Tokens that have some fixed string representation */
     val fixedTokens = Seq(Semantic, Define, Colon, Semicolon, Machine, Flow, TupleOpen, TupleClose, TupleSeparator)
     
+    /** Parser for [[fixedTokens]] */
     def fixedToken : Parser[Token] = fixedTokens.map(token => token.chars ^^^ token).reduceLeft(_ | _)
     
+    /** Parser for [[Token.Numeric]] [[Token.Identifier]]s */
     def numeric : Parser[Token] = """-?\d+""".r ^^ (new Identifier(_) with Numeric)
+    /** Parser for [[Token.Identifier]]s*/
     def identifier : Parser[Token] = """[A-Za-z0-9]+""".r ^^ (new Identifier(_))
     
+    /** Parser for single line [[Token.Comment]]s*/
     def singlelinecomment : Parser[Token] = """\/\/.*\r?\n""".r ^^ (Comment(_))
+    /** Parser for multiline [[Token.Comment]]s*/
     def multilinecomment : Parser[Token] = """\/\*((?!\*\/).|\r?\n)*\*\/""".r ^^ (Comment(_))
     
+    /** Parser for everything put together */
     def total : Parser[Seq[Token]] = (multilinecomment | singlelinecomment | fixedToken | numeric | identifier).*
 }
