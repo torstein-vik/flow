59cd6c4f35e11e0388ff71b1fad184d3b7e875b7
Torstein Vik <torsteinv64@gmail.com>
2017-12-03 04:38:20 +0100

    Implemented getTokens to use parseAll

--- src/main/scala/parser/lexer/Lexer.scala
+++ src/main/scala/parser/lexer/Lexer.scala
@@ -8,7 +8,9 @@ import scala.util.matching.Regex
 object Lexer extends RegexParsers {
     import Token._
     
-    def getTokens(data : String) : Seq[Token] = {
-        
+    def getTokens(data : String) : Seq[Token] = parseAll(total, data) match {
+        case Success(result, _) => result
+        case Failure(msg, _) => throw LexerException(msg)
+        case Error(msg, _) => throw LexerException(msg)
     }
 }
