8bec507ee9cd4f08e4257a5e115a3799d38ae177
Torstein Vik <torsteinv64@gmail.com>
2017-12-05 23:11:03 +0100

    Added doc to Block & Statement

--- src/main/scala/AST.scala
+++ src/main/scala/AST.scala
@@ -6,8 +6,12 @@ abstract sealed class AST
 
 /** Object with [[AST]] implementation */
 object AST {
+    /** A list of to [[Statement]]s to be 'exectued' 
+     *  @param stmts List of [[Statement]] that this block 'executes'
+     */
     case class Block(stmts : Statement*) extends AST
     
+    /** An abstact statement to be 'executed' */
     abstract sealed class Statement 
     case class Semantic(classes : parser.lexer.Token.Identifier*)
     case class Define(machine : parser.lexer.Token.Identifier, machinespec : MachineSpec)
