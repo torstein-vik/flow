aedbd5353fc38b57a28728911114f018faa9b17f
Torstein Vik <torsteinv64@gmail.com>
2017-12-06 22:23:46 +0100

    Added ParserException case class

--- src/main/scala/parser/ParserException.scala
+++ src/main/scala/parser/ParserException.scala
@@ -1,2 +1,6 @@
 package io.github.torsteinvik.flow.parser.lexer
 
+import scala.util.parsing.input._
+
+case class ParserException(msg : String, pos : Position) extends 
+    Exception("Syntax error at line " + pos.line + " column " + pos.column + "\n" + pos.longString)
