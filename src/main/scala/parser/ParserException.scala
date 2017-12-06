65edd79d8772b1c24757ac75836bbc78c85ae1c6
Torstein Vik <torsteinv64@gmail.com>
2017-12-06 22:23:57 +0100

    Added doc to ParserException

--- src/main/scala/parser/ParserException.scala
+++ src/main/scala/parser/ParserException.scala
@@ -2,5 +2,9 @@ package io.github.torsteinvik.flow.parser.lexer
 
 import scala.util.parsing.input._
 
+/** Exception coming from parsing the [[lexer.Token]]s into an [[AST]]
+ *  @param msg message of the exception
+ *  @param pos position in the input
+ */
 case class ParserException(msg : String, pos : Position) extends 
     Exception("Syntax error at line " + pos.line + " column " + pos.column + "\n" + pos.longString)
