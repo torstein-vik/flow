be5b33e732547ea755652c63f1b50cddc163140c
Torstein Vik <torsteinv64@gmail.com>
2017-12-06 22:25:40 +0100

    Added class TokenReader with stubs

--- src/main/scala/parser/TokenReader.scala
+++ src/main/scala/parser/TokenReader.scala
@@ -1,2 +1,12 @@
 package io.github.torsteinvik.flow.parser
 
+import io.github.torsteinvik.flow.parser.lexer._
+
+import scala.util.parsing.input._
+
+class TokenReader (tokens : Seq[Token]) extends Reader[Token] {
+    def atEnd: Boolean = ???
+    def first: Token = ???
+    def pos: Position = ???
+    def rest: TokenReader = ???
+}
