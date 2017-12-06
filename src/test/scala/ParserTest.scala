31e09948ab40d16474f82946f7e47dae8d3c900b
Torstein Vik <torsteinv64@gmail.com>
2017-12-06 19:46:23 +0100

    Added classes BasicParserTest and FileParserTest

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -1,4 +1,24 @@
 package io.github.torsteinvik.flow.test
 
+import io.github.torsteinvik.flow.parser.lexer.Token.Identifier
+import io.github.torsteinvik.flow.parser._
+import io.github.torsteinvik.flow._
 
 import org.scalatest.FunSuite
+
+class BasicParserTest extends FunSuite {
+    import AST._
+            
+    
+    
+    
+    
+    
+}
+
+class FileParserTest extends FunSuite {
+    import AST._
+    import scala.io.Source
+    
+    
+}
