d6cba22369cbeb19bc3ccee306140332afff4732
Torstein Vik <torsteinv64@gmail.com>
2017-12-06 23:18:22 +0100

    Fixed potential index out of bounds issue in TokenReader

--- src/main/scala/parser/TokenReader.scala
+++ src/main/scala/parser/TokenReader.scala
@@ -7,6 +7,6 @@ import scala.util.parsing.input._
 class TokenReader (tokens : Seq[Token], override val offset : Int = 0) extends Reader[Token] {
     def atEnd: Boolean = offset >= tokens.length
     def first: Token = if (offset < tokens.length) tokens(offset) else Token.EndToken
-    def pos: Position = tokens(offset).pos
+    def pos: Position = if (offset < tokens.length) tokens(offset).pos else NoPosition
     def rest: TokenReader = if (offset < tokens.length) new TokenReader(tokens, offset + 1) else this
 }
